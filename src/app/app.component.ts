715b20f75573b59488556693b6b489660d266433
angular-cli <angular-cli@angular.io>
2017-02-12 11:06:31 -0800

    chore: initial commit from angular-cli

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ng-seed-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'ng-seed works!';
+}
